f67ebd6c5c8c344ad2f61e70a67684186fb312b6
Andrew Haarsager <athaarsager@gmail.com>
2024-04-30 11:37:01 -0500

    Create API Project

---
+++ csharp-bookstore/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace csharp_bookstore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
